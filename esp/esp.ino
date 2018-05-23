b2d390cf3de59183940c502e81ed454ed96c2510
Srecko <srecko-95@hotmail.com>
2018-05-23 12:40:37 +0100

    Esp Kontroler

--- esp/esp.ino
+++ esp/esp.ino
@@ -1,10 +1,44 @@
 
-void setup() {
-  
+#include <ESP8266WiFi.h>
 
+#define pinOkidac 4
+
+WiFiClient client;
+WiFiServer server(80);
+
+const char* ssid = "Hackathon in the Port";
+const char* password = "";
+
+void setup() {
+  Serial.begin(115200);
+  pinMode(pinOkidac,OUTPUT);
+  digitalWrite(pinOkidac,HIGH);
+  connectWiFi();
+  server.begin();
 }
 
 void loop() {
   
+  client = server.available();
+  if(client.connected()){
+    digitalWrite(pinOkidac,LOW);
+    delay(1000);
+    digitalWrite(pinOkidac,HIGH);
+    }
 
+}
+
+void connectWiFi()
+{
+  Serial.println("Connecting to WIFI");
+  WiFi.begin(ssid, password);
+  while ((!(WiFi.status() == WL_CONNECTED)))
+  {
+    delay(300);
+    Serial.print("..");
+  }
+  Serial.println("");
+  Serial.println("WiFi connected");
+  Serial.println("NodeMCU Local IP is : ");
+  Serial.print((WiFi.localIP()));
 }
