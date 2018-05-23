80462176712d023ffe492083a795fa243500e43a
pyranth <pyranth95@hotmail.com>
2018-05-23 22:43:41 +0200

    php scripts for black

--- webapp/dbscripts/rooms.php
+++ webapp/dbscripts/rooms.php
@@ -2,7 +2,7 @@
 
 $mysqli = new mysqli('192.168.1.61','monty','some_pass','hackathon');
 $myArray = array();
-if ($result = $mysqli->query("SELECT * FROM hotel")) {
+if ($result = $mysqli->query("SELECT * FROM room")) {
 
     while($row = $result->fetch_array(MYSQL_ASSOC)) {
             $myArray[] = $row;
