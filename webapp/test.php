74fa2e34bda1eb2de43f910ccb58f166d6e0c40e
pyranth <pyranth95@hotmail.com>
2018-05-23 21:10:25 +0200

    Php ne valja

---
+++ webapp/test.php
@@ -0,0 +1,25 @@
+<?php
+
+$db = new mysqli('10.71.9.138', 'monty', 'some_pass', 'hackathon');
+
+if ($db->connect_error){
+	die("error");
+}
+
+$query = $db->query('DESCRIBE `hotel`');
+$fields = array();
+while($row = $query->fetch_assoc()) {
+    $fields[] = $row['Field'];
+}
+
+?>
+
+<form id="generate-form" type="POST">
+    <?php foreach($fields as $field): ?>
+        <label>
+            <?php echo "$field: "; ?>
+            <input type="text" name="<?php echo $field; ?>" />
+        </label><br/>
+    <?php endforeach; ?>
+    <input type="submit" name="submit" />
+</form>
