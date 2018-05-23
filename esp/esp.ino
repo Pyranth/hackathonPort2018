32696bd04dce183ccb41c0de7f9af67b97f86e60
Dejan <ca_na@outlook.com>
2018-05-24 01:21:03 +0200

    Jos koda

--- esp/esp.ino
+++ esp/esp.ino
@@ -1,31 +1,58 @@
 
 #include <ESP8266WiFi.h>
 
-#define pinOkidac 4
-
+#define pinOkidac 4 // pin d2
+#define pinDiodaZelena 0 //pin d3
+#define pinDiodaCrvena 2 //pin d3
 WiFiClient client;
 WiFiServer server(80);
 
-const char* ssid = "Hackathon in the Port";
-const char* password = "";
+const char* ssid = "Sea Point Apartments";
+const char* password = "biljana123";
+char data;
 
 void setup() {
   Serial.begin(115200);
   pinMode(pinOkidac,OUTPUT);
+  pinMode(pinDiodaZelena,OUTPUT);
+  pinMode(pinDiodaCrvena,OUTPUT);
   digitalWrite(pinOkidac,HIGH);
+  digitalWrite(pinDiodaZelena,LOW);
+  digitalWrite(pinDiodaCrvena,LOW);
   connectWiFi();
   server.begin();
 }
 
 void loop() {
-  
-  client = server.available();
-  if(client.connected()){
-    digitalWrite(pinOkidac,LOW);
-    delay(1000);
-    digitalWrite(pinOkidac,HIGH);
-    }
 
+  client = server.available();
+        //Serial.println((WiFi.localIP()));
+
+  while(client.connected()){
+    if (client.available()>0) { 
+      
+      data=client.read();
+      Serial.println(data);
+      if(data=='Z'){
+        digitalWrite(pinDiodaZelena,HIGH);
+      }
+      if(data=='z'){
+        digitalWrite(pinDiodaZelena,LOW);
+      }
+      if(data=='C'){
+        digitalWrite(pinDiodaCrvena,HIGH);
+      }
+      if(data=='c'){
+        digitalWrite(pinDiodaCrvena,LOW);
+      }
+      if(data=='b'){
+        digitalWrite(pinDiodaZelena,HIGH);
+        digitalWrite(pinOkidac,LOW);
+        delay(500);
+        digitalWrite(pinOkidac,HIGH );
+      }
+    }
+  }
 }
 
 void connectWiFi()
@@ -40,5 +67,5 @@ void connectWiFi()
   Serial.println("");
   Serial.println("WiFi connected");
   Serial.println("NodeMCU Local IP is : ");
-  Serial.print((WiFi.localIP()));
+  Serial.println((WiFi.localIP()));
 }
---
+++ webapp/dbscripts/reservations.php
@@ -0,0 +1,16 @@
+<?php
+
+$mysqli = new mysqli('192.168.1.61','monty','some_pass','hackathon');
+$myArray = array();
+if ($result = $mysqli->query("SELECT * FROM reservation")) {
+
+    while($row = $result->fetch_array(MYSQL_ASSOC)) {
+            $myArray[] = $row;
+    }
+    echo json_encode($myArray);
+}
+
+$result->close();
+$mysqli->close();
+
+?>
